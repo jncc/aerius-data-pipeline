9d3e94d12071e01154d67b137a8381bbbd0a6ef0
Kieran Fox <46992478+foxfoxfox7@users.noreply.github.com>
2022-03-07 11:53:38 +0000

    converting to local package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='aeriusdatapipeline',
+      version='0.1',
+      description='ETL code for the UK AERIUS project.',
+      author='Kieran Fox',
+      license='OGL',
+      packages=['aeriusdatapipeline'],
+      zip_safe=False
+      )
