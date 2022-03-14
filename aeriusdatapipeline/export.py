c03cc3dbaceccce46fffc124ea4dbbc2a2e85eff
Kieran Fox <46992478+foxfoxfox7@users.noreply.github.com>
2022-03-14 21:20:42 +0000

    removed the export only class

--- aeriusdatapipeline/export.py
+++ aeriusdatapipeline/export.py
@@ -1,18 +1,6 @@
 from pathlib import Path
 
 
-class ExportData():
-    """Exporter for data structures into txt files for db.
-    Input the overall output path
-    """
-
-    def export_data(self, filepath):
-        root = f'./output/{filepath}/'
-        print(f'Exporting file {self.name} to {root}\n')
-        Path(root).mkdir(parents=True, exist_ok=True)
-        self.data.to_csv(f'{root}/{self.name}.txt', sep='\t', index=False)
-
-
 class Table():
     """Exporter for data structures into txt files for db.
     Input the overall output path
